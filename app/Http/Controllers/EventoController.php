10b7abaf53f1ac51680f668d47572ceae99927ad
gchang110101 <gdchang@uamv.edu.ni>
2024-09-11 22:29:34 -0600

    segunda subida de archivos

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -12,25 +12,33 @@ class EventoController extends Controller
      */
     public function index()
     {
-        $eventos = Evento::orderBy('facultad', 'asc')
-        ->orderBy('fecha_hora_inicio', 'desc')
-        ->orderBy('fecha_hora_fin', 'desc')
-        //->orderBy('deporte', 'asc')
-        ->orderBy('tipo_evento', 'asc')
-        ->orderBy('estado', 'asc')
-        //->orderBy('capacidad', 'asc')
-        //->orderBy('organizador', 'asc')
-        ->get();
 
-        return response()->json(
-            [
-                'status' => 'sucess',
-                'data' => $eventos,
-            ],
-            200,
-            [],
-            JSON_PRETTY_PRINT
-        );
+        try {
+            $eventos = Evento::orderBy('facultad', 'asc')
+            ->orderBy('fecha_hora_inicio', 'desc')
+            ->orderBy('fecha_hora_fin', 'desc')
+            //->orderBy('deporte', 'asc')
+            ->orderBy('tipo_evento', 'asc')
+            ->orderBy('estado', 'asc')
+            //->orderBy('capacidad', 'asc')
+            //->orderBy('organizador', 'asc')
+            ->get();
+
+            return response()->json(
+                [
+                    'status' => 'sucess',
+                    'data' => $eventos,
+                ],
+                200,
+                [],
+                JSON_PRETTY_PRINT
+            );
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'data' => $e->getMessage()
+            ]);
+        }
     }
 
     /**
@@ -38,15 +46,47 @@ class EventoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $evento_request_body = $request->all();
+
+            $evento_post = Evento::create($evento_request_body);
+
+            return response()->json([
+                'status' => 'se guardó el evento satisfactoriamente',
+                'request_date_time' => date('Y-m-d H:i:s'),
+                'data' => $evento_post
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'data' => $e->getMessage()
+            ]);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $value)
     {
-        //
+        try {
+            $cad = '%' . $value . '%';
+
+            $eventos_search_by_title_description = Evento::where('titulo', 'LIKE', $cad)
+                ->orWhere('descripcion', 'LIKE', $cad)
+                ->get();
+
+            return response()->json([
+                'status' => 'succes',
+                'request_date_time' => date('Y-m-d H:i:s'),
+                'data' => $eventos_search_by_title_description
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'data' => $e->getMessage()
+            ]);
+        }
     }
 
     /**
--- app/Models/Evento.php
+++ app/Models/Evento.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Evento extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'titulo',
+        'descripcion',
+        'fecha_hora_inicio',
+        'fecha_hora_fin',
+        'facultad',
+        'ubicacion',
+        'deporte',
+        'organizador',
+        'capacidad',
+        'tipo_evento',
+        'requiere_inscripcion',
+        'estado',
+        'etiquetas'
+    ];
 }
